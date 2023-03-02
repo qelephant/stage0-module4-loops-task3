f2a4655129fc5de2007a7e5188835d9e0fe96394
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-02 17:05:52 +0600

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
-        int n = lastFibonacci, firstTerm = 0, secondTerm = 1;
-        for (int i = 1; i <= n; ++i) {
+        int firstTerm = 0, secondTerm = 1;
+        for (int i = 1; i <= lastFibonacci; ++i) {
           System.out.print(firstTerm + ", ");
           int nextTerm = firstTerm + secondTerm;
           firstTerm = secondTerm;
